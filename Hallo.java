5e12bf74d85f04b9a68f432ac3bfb0e541498e81
Anggi Riyandi <anggi.riyandi@gmail.com>
2017-11-25 10:54:49 +0700

    initial commit

---
+++ Hallo.java
@@ -0,0 +1,11 @@
+public class Hallo {
+	private String nama = "anggi";
+
+	public void salam() {
+		System.out.println("Hallo : "+ nama);
+	}
+
+	public static void main(String arr[]){
+		System.out.println("Hallo !!");
+	}
+}
